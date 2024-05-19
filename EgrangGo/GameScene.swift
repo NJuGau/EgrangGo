e8917fb8fa6b6254944963efab5bdfba6b183ee1
NJuGau <nathanael.gauthama@binus.ac.id>
2024-05-19 23:49:42 +0700

    Add Joystick, Make Player movement concept, add camera (unusable)

--- EgrangGo/GameScene.swift
+++ EgrangGo/GameScene.swift
@@ -14,9 +14,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     let playerRightLeg = PlayerLeg()
     let ground = SKShapeNode(rectOf: CGSize(width: 1024, height: 200))
     
+    let joystickLeft = TLAnalogJoystick(withDiameter: 200)
+    let joystickRight = TLAnalogJoystick(withDiameter: 200)
+    
+    let cam = SKCameraNode()
+    
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
-//        physicsWorld.gravity = .zero
+        
+        physicsWorld.gravity = CGVectorMake(0, -0.5)
         if let leaf = SKEmitterNode(fileNamed: "LeafBlowParticle"){
             leaf.position = CGPoint(x:1080, y: 0)
             leaf.zPosition = -1
@@ -35,6 +41,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 //        playerRightLeg.anchorPoint = CGPoint(x: 0.3, y: 0.85)
 //        playerRightLeg.zRotation = -90
         
+        //starting rotation
+        
         addChild(playerBody)
         playerBody.addChild(playerLeftLeg)
         playerBody.addChild(playerRightLeg)
@@ -51,20 +59,42 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         ground.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 1024, height: 200))
         ground.physicsBody?.affectedByGravity = false
         ground.physicsBody?.isDynamic = false
+        ground.physicsBody?.friction = 1
         ground.position.x = 0
         ground.position.y = -frame.height / 2
         addChild(ground)
+        
+        joystickLeft.position = CGPoint(x: 1024 * -0.5 + joystickLeft.radius + 50, y: 768 * -0.5 + joystickLeft.radius + 50)
+        joystickLeft.zPosition = 5
+        joystickLeft.baseImage = UIImage(named: "joystick_back")
+        joystickLeft.handleImage = UIImage(named: "joystick_front")
+        addChild(joystickLeft)
+        joystickLeft.on(.move) { [unowned self] data in
+            playerLeftLeg.zRotation = data.angular + .pi
+            print("\(data.angular)")
+        }
+        
+        joystickRight.position = CGPoint(x: 1024 * 0.5 - joystickRight.radius - 50, y: 768 * -0.5 + joystickRight.radius + 50)
+        joystickRight.zPosition = 5
+        joystickRight.baseImage = UIImage(named: "joystick_back")
+        joystickRight.handleImage = UIImage(named: "joystick_front")
+        addChild(joystickRight)
+        joystickRight.on(.move) { [unowned self] jsData in
+            playerRightLeg.zRotation = jsData.angular + .pi
+            print("moving: \(jsData.angular)")
+            
+        }
+        
+//        cam.setScale(2)
+//        self.camera = cam
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+//        cam.position = playerBody.position
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        playerLeftLeg.run(SKAction.rotate(byAngle: .pi / 2, duration: 1.0))
-        playerRightLeg.run(SKAction.rotate(byAngle: -.pi / 2, duration: 1.0))
-    }
-    
-    func setCamera() {
-        let camera = SKCameraNode()
-        camera.setScale(1)
-        addChild(camera)
-        scene!.camera = camera
+//        playerLeftLeg.run(SKAction.rotate(byAngle: .pi / 2, duration: 1.0))
+//        playerRightLeg.run(SKAction.rotate(byAngle: -.pi / 2, duration: 1.0))
     }
 }
